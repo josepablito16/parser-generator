648ca4bffe4e8d1f4d7ae798566a4d865b2abbf8
josepablito16 <josepablocif16@gmail.com>
2021-05-30 22:09:52 -0600

    Se finalizan las pruebas de preprocesamiento

--- basicTest.py
+++ basicTest.py
@@ -70,9 +70,12 @@ assert getTest([Character('L'), '{', Character('L'), '|', Character('D'), '}']) 
 assert getTest([Character('"'), Character('S'), '{', Character('S'), '}', Character('"')]) == ['LPAREN', 'LPAREN', 'LPAREN', {
     '"'}, 'CONCAT', {'S'}, 'RPAREN', 'CONCAT', 'LPAREN', {'S'}, 'MUL', 'ALFA', 'RPAREN', 'RPAREN', 'CONCAT', {'"'}, 'RPAREN']
 
+
+assert getTest([Character('"'), '[', Character('/'), ']', Character('S'), Character('"')]) == ['LPAREN', 'LPAREN', 'LPAREN',
+                                                                                               {'"'}, 'CONCAT', 'LPAREN', {'/'}, 'OR', 'EPSILON', 'RPAREN', 'RPAREN', 'CONCAT', {'S'}, 'RPAREN', 'CONCAT', {'"'}, 'RPAREN']
 '''
-print(getTest([Character('"'), Character('S'),
-               '{', Character('S'), '}', Character('"')]))
+print(getTest([Character('C('), Character('D'), '{', Character('D'), '}', Character(')'), Character(
+    '..'), Character('C('), Character('D'), '{', Character('D'), '}', Character(')')]))
 '''
 
 print("TODO BIEN!")
